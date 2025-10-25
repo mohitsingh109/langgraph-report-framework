7dcc7058c51ea01417d7abca9a924c429df8cbdb
Homely Assist <homeconnecteasy@gmail.com>
2025-10-25 22:14:02 +0530

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,24 @@
+def main():
+    print("Hello from app!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""Few Template json"""
+"""
+{
+    "id": <uuid>,
+    "name": "Hello Template",
+    "config": {
+        ...
+    },
+    ...
+}
+"""
+
+"""
+    Required field for specific template,
+    Required field to schedule a report 
+"""
